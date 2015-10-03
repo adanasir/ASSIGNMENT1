e0a2e183f686ed32f2f03cf51f1bdc5143c4bbb5
adanasir <adanasir@gmail.com>
2015-10-03 12:02:04 +0800

    create folder for java n python

---
+++ python/asgmt1q1adanasir.py
@@ -0,0 +1,17 @@
+
+
+file = open("adanasirpy.txt", "w")
+
+file.write("Name: Siti Aminah Shuhada Mat Nasir\n")
+
+file.write("Matrix No.: 2014433638\n")
+
+file.write("Gender: Female\n")
+
+file.write("Age: 32 years old\n")
+
+file.write("E-mails: adanasir@gmail.com\n")
+
+file.write("Mobile Phone: 0199171336\n")
+
+file.close()
---
+++ python/asgmt1q2.py
@@ -0,0 +1,5 @@
+
+
+fh = open("adanasirpy.txt", "r")
+print (fh.read())
+fh.close
